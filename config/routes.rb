0cbeb446bd5091a7e03201fe3c1fbadd14519598
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-04-12 06:58:55 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "homes#top"
+
+  resources :books, only: [:new, :create]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
